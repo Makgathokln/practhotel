128c7cfe78575f1e2bc8ae9979517fb7be39cb57
Makgathokln <89177366+Codetribers@users.noreply.github.com>
2022-01-18 10:23:19 +0200

    commit

---
+++ components/screens/PaymentView.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+
+const PaymentView = () =>{
+    return(
+        <View style={styles.navigation}>
+                <Text>Navigation</Text>
+        </View>
+        
+    )
+}
+export default PaymentView
